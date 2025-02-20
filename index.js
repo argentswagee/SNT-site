774aa450ca2babd4c12852f776a053352107ea20
Edouard Auchet <125192936+argentswagee@users.noreply.github.com>
2025-02-20 02:30:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ document.getElementById('cookiea').addEventListener('click', cookie);
 
 document.getElementById('gt3rs').addEventListener('click', gt3rs);
 function gt3rs() {
-    window.location.href = 'gt3rs/gt3rs.html';
+    window.location.href = 'gt3rs.html';
 }
 
 document.getElementById('cayenne').addEventListener('click', cayenne);
 function cayenne() {
-    window.location.href = 'cayenne/cayenne.html';
+    window.location.href = 'cayenne.html';
 }
 
 document.getElementById('911').addEventListener('click', nevada);
 function nevada() {
-    window.location.href = '911/911.html';
+    window.location.href = '911.html';
 }
