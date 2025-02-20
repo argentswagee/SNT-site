714a13ff2dc952e45ea3469acd81796e9840fe45
Edouard Auchet <125192936+argentswagee@users.noreply.github.com>
2025-02-20 02:26:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function cookie() {
+    document.getElementById("footer").style.display = 'none';
+}
+document.getElementById('cookier').addEventListener('click', cookie);
+document.getElementById('cookiea').addEventListener('click', cookie);
+
+
+document.getElementById('gt3rs').addEventListener('click', gt3rs);
+function gt3rs() {
+    window.location.href = 'gt3rs/gt3rs.html';
+}
+
+document.getElementById('cayenne').addEventListener('click', cayenne);
+function cayenne() {
+    window.location.href = 'cayenne/cayenne.html';
+}
+
+document.getElementById('911').addEventListener('click', nevada);
+function nevada() {
+    window.location.href = '911/911.html';
+}
